92c1b456ea6558d235f5e84495f7ce1a8e4c8e3a
0xmikki <90579247+MrBloomguy@users.noreply.github.com>
2025-02-10 13:32:24 +0000

    update-1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      buffer: 'buffer/',
+      buffer: 'buffer',
       process: 'process/browser',
       stream: 'stream-browserify',
       crypto: 'crypto-browserify'
